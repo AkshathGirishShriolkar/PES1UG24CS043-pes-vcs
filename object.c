393921de7b4f9b3ab6bb1e1c813be6959f8aadaf
AkshathGirishShirolkar <shirolkarakshath@gmail.com>
2026-04-20 12:39:10 +0530

    phase1: load object and verify hash integrity

--- object.c
+++ object.c
@@ -210,7 +210,43 @@ int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out
 // The caller is responsible for calling free(*data_out).
 // Returns 0 on success, -1 on error (file not found, corrupt, etc.).
 int object_read(const ObjectID *id, ObjectType *type_out, void **data_out, size_t *len_out) {
-    // TODO: Implement
-    (void)id; (void)type_out; (void)data_out; (void)len_out;
+    char path[512];
+    object_path(id, path, sizeof(path));
+
+    FILE *fp = fopen(path, "rb");
+    if (!fp) return -1;
+
+    fseek(fp, 0, SEEK_END);
+    long file_size_long = ftell(fp);
+    if (file_size_long <= 0) {
+        fclose(fp);
+        return -1;
+    }
+    rewind(fp);
+
+    size_t file_size = file_size_long;
+    unsigned char *buffer = malloc(file_size);
+    if (!buffer) {
+        fclose(fp);
+        return -1;
+    }
+
+    if (fread(buffer, 1, file_size, fp) != file_size) {
+        free(buffer);
+        fclose(fp);
+        return -1;
+    }
+
+    fclose(fp);
+
+    ObjectID computed;
+    compute_hash(buffer, file_size, &computed);
+
+    if (memcmp(&computed, id, sizeof(ObjectID)) != 0) {
+        free(buffer);
+        return -1;
+    }
+
+    free(buffer);
     return -1;
 }
