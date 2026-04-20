4ad3496be7c410804ea040993de958f328ed9342
AkshathGirishShirolkar <shirolkarakshath@gmail.com>
2026-04-20 12:22:22 +0530

    phase1: serialize object and compute hash

--- object.c
+++ object.c
@@ -94,7 +94,27 @@ int object_exists(const ObjectID *id) {
 //
 // Returns 0 on success, -1 on error.
 int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out) {
-    // TODO: Implement
+    const char *type_str;
+
+    if (type == OBJ_BLOB) type_str = "blob";
+    else if (type == OBJ_TREE) type_str = "tree";
+    else if (type == OBJ_COMMIT) type_str = "commit";
+    else return -1;
+
+    char header[64];
+    int header_len = snprintf(header, sizeof(header), "%s %zu", type_str, len);
+    if (header_len < 0 || (size_t)header_len >= sizeof(header)) return -1;
+
+    size_t full_len = (size_t)header_len + 1 + len;
+
+    unsigned char *full_obj = malloc(full_len);
+    if (!full_obj) return -1;
+
+    memcpy(full_obj, header, header_len);
+    full_obj[header_len] = '\0';
+    memcpy(full_obj + header_len + 1, data, len);
+
+    compute_hash(full_obj, full_len, id_out);
     (void)type; (void)data; (void)len; (void)id_out;
     return -1;
 }
